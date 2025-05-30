07a5185b71e9fbf49772e4958db789d6afc1cf28
ADITYA PRATAP SINGH TOMAR <aditya.pratap.singh.tomar.1082006@gmail.com>
2025-05-30 16:24:47 +0530

    updating interview module

--- interviewer.py
+++ interviewer.py
@@ -10,7 +10,8 @@ strOutputParser = StrOutputParser()
 system_prompt = PromptTemplate( 
     input_variables=["candidate_info", "resume_text", "job_description"],
     template=(
-            "You are a professional interviewer going to take interview for the position described below. \n" 
+            "You are a professional interviewer bot going to take interview for the position described below. \n" 
+            "Candidate responses will be going to add into chat history one by one \n"
             "You have ask first question, start with ice breaker or intro question based on the following information.\n"
             "Candidate Information:\n{candidate_info}\n"
             "Resume:\n{resume_text}\n"
@@ -25,14 +26,14 @@ question_prompt = PromptTemplate(
             "You are a professional interviewer taking interview \n"
             "Here is the whole chat of the interview untill now: {messages}\n"
             "You have just ask next question."
-            "These are the rules you have to follow while asking for a question:\n"
-            "Wait for candidate to answer the question before asking the next question.\n"
-            "Take the most important topic relevant for a job and based on resume and start easy questions\n"
-            "If candidate is answering correctly, gradually increase the difficulty of the next question.\n"
-            "If candidate is struggling, ask simpler or clarifying questions.\n"
-            "Vary the question types (theory, practical, scenario-based, etc.).\n"
-            "Move to next topic when the candidate has demonstrated sufficient knowledge in the current topic or candidate appologised that he/she is unable to answer this question.\n"
-            "If candidate is unable to answer a question correctly, ask a related simpler question to help them get back on track. If he/she is fully wrong, go to next topic and sweetly tell him/her that you are wrong\n"
+            "These are the rules you have to take care while asking for next question or while analysing the responses:\n"
+            "- Wait for candidate to answer the question before asuuming anything or asking the next question.\n"
+            "- Take the most important topic relevant for a job and based on resume and start easy questions\n"
+            "- If candidate is answering correctly, gradually increase the difficulty of the next question.\n"
+            "- If candidate is struggling, ask simpler or clarifying questions.\n"
+            "- Vary the question types (theory, practical, scenario-based, etc.).\n"
+            "- Move to next topic when the candidate has demonstrated sufficient knowledge in the current topic or candidate appologised that he/she is unable to answer this question.\n"
+            "- If candidate is unable to answer a question correctly, ask a related simpler question to help them get back on track. If he/she is fully wrong, go to next topic and sweetly tell him/her that you are wrong\n"
             "Be professional and concise.\n"
             "Start the interview now."
         ),
