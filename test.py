0466304cbd9df550a30b675b973a54b5f7ec3892
ADITYA PRATAP SINGH TOMAR <aditya.pratap.singh.tomar.1082006@gmail.com>
2025-05-31 08:52:59 +0530

    Added gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
-model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key="AIzaSyCil3yELy5_mHhdCB4sOjsrVQXYBUha3Po")
-
-result = model.invoke("List the top 5 skills needed for a data scientist, ordered by importance.")
-print(result.content)  
